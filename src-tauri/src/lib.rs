bc05f087a2d222740afd7083630ed50706016cb2
Godruoyi <godruoyi@gmail.com>
2025-07-11 08:44:21 +0800

    add uploader

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,6 +22,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
             None,
